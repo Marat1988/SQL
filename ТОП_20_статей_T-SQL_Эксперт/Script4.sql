822b8ea10778774d9e8c95a62105035dd7612602
Marat <pirat03071988@mail.ru>
2024-08-17 19:53:19 +0200

    In-Memory OLTP

---
+++ ТОП_20_статей_T-SQL_Эксперт/Script4.sql
@@ -0,0 +1,34 @@
+﻿--Создание обычной базы данных для тестов
+CREATE DATABASE DBInMemory;
+GO
+--Переходим в контекст новой базы данных
+USE DBInMemory
+GO
+--Создаем файловую группу, оптимизированную для памяти
+ALTER DATABASE DBInMemory ADD FILEGROUP FileGroupInMemory CONTAINS MEMORY_OPTIMIZED_DATA;
+GO
+
+--Добавляем контейнер в файловую группу
+ALTER DATABASE DBInMemory ADD FILE
+(
+	name='FileMemory',
+	filename='C:\DataBase\FileInMemory'
+)
+TO FILEGROUP FileGroupInMemory;
+
+--Создаем таблицу, оптимизированную для памяти
+CREATE TABLE TestTableInMemory
+(
+	ProductId INT IDENTITY(1,1) PRIMARY KEY NONCLUSTERED,
+	ProductName NVARCHAR(100) NOT NULL,
+	Price MONEY
+) WITH (MEMORY_OPTIMIZED=ON); --Параметр для создания таблицы, оптимизированной для памяти
+GO
+
+--Добавляем данные в таблицу
+INSERT INTO TestTableInMemory
+VALUES ('Тестовая строка', 100);
+GO
+--Проверяем
+SELECT *
+FROM TestTableInMemory
