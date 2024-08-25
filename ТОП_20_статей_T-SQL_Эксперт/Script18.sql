3f23c03f1b2cb8293ba8aac40b62545e6174c23f
Marat <pirat03071988@mail.ru>
2024-08-25 20:28:59 +0200

    Дата и время запуска сервера

---
+++ ТОП_20_статей_T-SQL_Эксперт/Script18.sql
@@ -0,0 +1,10 @@
+﻿/*Статья https://info-comp.ru/how-to-find-launch-date-ms-sql-server*/
+
+SELECT sqlserver_start_time
+FROM sys.dm_os_sys_info
+
+SELECT create_date
+FROM sys.databases
+WHERE name='tempdb'
+
+EXEC sp_readerrorlog 0,1,'SQL Server is Starting'
