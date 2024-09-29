004a33b61f30f6aaaaae573e5aa266882f3fa4b0
Marat <pirat03071988@mail.ru>
2024-09-29 13:03:00 +0200

    Вычисляемые столбцы SQL

---
+++ Сайт info-comp.ru/ТОП_30_статей_T_SQL_Начинающий/Script9.sql
@@ -0,0 +1,37 @@
+﻿/*Статья https://info-comp.ru/obucheniest/623-calculated-columns-in-t-sql.html*/
+
+USE TestDB;
+GO
+
+DROP TABLE IF EXISTS TestTable;
+GO
+
+--Создание таблицы с вычисляемым столбцом
+CREATE TABLE TestTable
+(
+	ProductId INT IDENTITY(1,1) NOT NULL,
+	ProductName VARCHAR(100) NOT NULL,
+	Quantity SMALLINT NULL,
+	Price MONEY NULL,
+	Summa AS ([Quantity]*[Price]) PERSISTED --Вычисляемый столбец
+)
+
+--Добавление данных в таблицу
+INSERT INTO TestTable
+VALUES ('Портфель', 1, 500),
+	   ('Карандаш', 5, 20),
+	   ('Тетрадь', 10, 50);
+GO
+
+--Выборка данных
+SELECT *
+FROM TestTable
+
+--Добавление вычисляемого столбца в таблицу
+ALTER TABLE TestTable ADD SummaALL AS ([Quantity] * [Price] * 1.7);
+
+--Выборка данных
+SELECT *
+FROM TestTable
+
+ALTER TABLE TestTable DROP COLUMN SummaALL;
