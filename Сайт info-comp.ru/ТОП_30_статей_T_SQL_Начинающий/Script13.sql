ba0c597249ccc061a7c8ad190b9119cd2480a5cb
Marat <pirat03071988@mail.ru>
2024-09-30 20:41:58 +0200

    EXISTS

---
+++ Сайт info-comp.ru/ТОП_30_статей_T_SQL_Начинающий/Script13.sql
@@ -0,0 +1,12 @@
+﻿/*Статья https://info-comp.ru/programmirovanie/691-exists-in-t-sql.html*/
+
+INSERT INTO TestTable2(Id, IdProperty, IsEnabled, IsTest)
+VALUES (1,1,1,1)
+
+
+SELECT *
+FROM TestTable
+WHERE EXISTS(SELECT * FROM TestTable2)
+
+IF NOT EXISTS(SELECT * FROM TestTable WHERE ProductId > 3)
+	SELECT 'Вложенный запрос не возвращает строк.' AS [Результат]
