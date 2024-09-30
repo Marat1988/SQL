133c48339916b7f7ab8124f8524ee45f8e1d7468
Marat <pirat03071988@mail.ru>
2024-09-30 20:48:05 +0200

    USE SQL

---
+++ Сайт info-comp.ru/ТОП_30_статей_T_SQL_Начинающий/Script14.sql
@@ -0,0 +1,17 @@
+﻿/*Статья https://info-comp.ru/obucheniest/648-command-use-in-t-sql.html*/
+
+USE TestDB
+GO
+
+--Инструкции для базы данных TestDB
+SELECT DB_NAME() AS [Имя базы данных]
+
+USE master
+GO
+--Инструкции для базы данных master
+SELECT DB_NAME() AS [Имя базы данных]
+
+USE master
+GO
+SELECT *
+FROM TestDB.dbo.TestTable
