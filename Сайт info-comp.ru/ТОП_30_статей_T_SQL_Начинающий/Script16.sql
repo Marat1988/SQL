503e6db56b640bb0d4eb5e453d573da1f1052776
Marat <pirat03071988@mail.ru>
2024-10-01 20:38:36 +0200

    TRUNCATE

---
+++ Сайт info-comp.ru/ТОП_30_статей_T_SQL_Начинающий/Script16.sql
@@ -0,0 +1,38 @@
+﻿/*Статья https://info-comp.ru/programmirovanie/580-truncate-table-ms-sql-server.html*/
+
+DROP TABLE IF EXISTS TestTable;
+GO
+
+--Создаем тестовую таблицу
+CREATE TABLE TestTable
+(
+	ProductId INT IDENTITY(1,1) NOT NULL,
+	ProductName VARCHAR(100) NOT NULL
+);
+GO
+
+--Добавляем данные
+INSERT INTO TestTable
+VALUES ('Компьютер'),
+	   ('Монитор'),
+	   ('Принтер');
+GO
+
+--Выборка данных
+SELECT *
+FROM TestTable;
+
+--Удаляем все данные инструкцией TRUNCATE TABLE
+TRUNCATE TABLE TestTable;
+GO
+
+--Снова созраняем данные
+INSERT INTO TestTable
+VALUES ('Компьютер'),
+	   ('Монитор'),
+	   ('Принтер');
+GO
+
+--Выборка данных
+SELECT *
+FROM TestTable;
