1a15e0cd0ba2fb2c4cabd935a4ab83c24bbb12f9
Marat <pirat03071988@mail.ru>
2024-10-05 15:00:29 +0200

    OFFSET FETCH SQL

---
+++ Сайт info-comp.ru/ТОП_30_статей_T_SQL_Начинающий/Script28.sql
@@ -0,0 +1,38 @@
+﻿DROP TABLE IF EXISTS TestTable;
+GO
+
+--Создание таблицы
+CREATE TABLE TestTable
+(
+	ProductId INT IDENTITY(1,1) NOT NULL,
+	ProductName VARCHAR(100) NOT NULL,
+	Price MONEY NULL
+);
+GO
+
+--Добавление строк в таблицу
+INSERT INTO TestTable (ProductName, Price)
+VALUES ('Системный блок', 300),
+		('Монитор', 200),
+		('Клавиатура', 100),
+		('Мышь', 50),
+		('Принтер', 200),
+		('Сканер', 150),
+		('Телефон', 250),
+		('Планшет', 300)
+--Выборка данных
+SELECT *
+FROM TestTable
+
+--Пропуск первых 3 строк
+SELECT *
+FROM TestTable
+ORDER BY ProductId
+OFFSET 3 ROWS
+
+--Пропуск первых 3 строк и возвращение следующих 3
+SELECT *
+FROM TestTable
+ORDER BY ProductId
+OFFSET 3 ROWS FETCH NEXT 3 ROWS ONLY
+
