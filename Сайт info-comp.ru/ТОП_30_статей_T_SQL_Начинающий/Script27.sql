28f67aba19935a1aa852956326231cbcd007c4b5
Marat <pirat03071988@mail.ru>
2024-10-05 14:44:29 +0200

    DROP IF EXISTS T-SQL

---
+++ Сайт info-comp.ru/ТОП_30_статей_T_SQL_Начинающий/Script27.sql
@@ -0,0 +1,15 @@
+﻿/*Статья https://info-comp.ru/obucheniest/683-drop-if-exists-in-t-sql.html*/
+
+IF OBJECT_ID('dbo.TestTable', 'U') IS NOT NULL
+	DROP TABLE TestTable;
+GO
+
+DROP TABLE IF EXISTS dbo.TestTable;
+GO
+
+IF EXISTS(SELECT *
+		  FROM sys.objects
+		  WHERE object_id=OBJECT_ID(N'dbo.TestFunction') AND type IN (N'FN', N'IF', N'TF', N'FS', N'FT'))
+	DROP FUNCTION TestFunction;
+
+DROP FUNCTION IF EXISTS TestFunction;
